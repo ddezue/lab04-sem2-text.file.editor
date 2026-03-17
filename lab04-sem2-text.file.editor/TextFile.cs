190cf4a5fe078fe9b90db208312ed0e4684c9ec5
dezue <mistfoll@mail.ru>
2026-03-17 15:09:09 +0700

    added classes

---
+++ lab04-sem2-text.file.editor/TextFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04_sem2_text.file.editor
+{
+  internal class TextFile
+  {
+  }
+}
